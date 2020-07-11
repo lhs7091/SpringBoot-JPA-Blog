8d6b44732f66ecfc440b942b49f9cdc13b8d7ae3
lhs7091 <lhs7091@naver.com>
2020-07-11 17:40:00 +0900

    yml test

--- src/main/java/com/cos/blog/test/Member.java
+++ src/main/java/com/cos/blog/test/Member.java
@@ -1,10 +1,8 @@
 package com.cos.blog.test;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 //@Getter
 //@Setter
