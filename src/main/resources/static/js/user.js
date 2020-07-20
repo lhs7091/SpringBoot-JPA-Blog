f74034fd263eb53962c2b77a611e99d147e7a462
lhs7091 <lhs7091@naver.com>
2020-07-20 11:40:30 +0900

    sign up function by service class

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,6 +1,6 @@
 let index={
     init: function(){
-        $("#btn-save").on("click", ()=>{
+        $("#btn-save").on("click", ()=>{ // function(){}, ()=> for this binding
             this.save();
         });
     },
@@ -12,12 +12,29 @@ let index={
             username: $("#username").val(),
             password: $("#password").val(),
             email: $("#email").val()
-        }
+        };
 
         //console.log(data);
 
         // 3 parameters change to json and request insert query by ajax communication
-        $.ajax().done().fail();
+        // ajax call : default = ansync
+        // return dataType: default = json
+        $.ajax({
+            // request sign up
+            type: "POST",
+            url: "/blog/api/user",
+            data: JSON.stringify(data), // http body data
+            contentType: "application/json; charset=utf-8", // body data type
+            dataType: "json" //when comming response, String type, json format to javascript format
+        }).done(function(resp){
+            // success
+            alert("complete of sign up");
+            console.log(resp);
+            location.href="/blog";
+        }).fail(function(){
+            // fail 
+            alert(JSON.stringify(error));
+        });
     }
 }
 
